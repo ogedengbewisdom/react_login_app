1427d821eef6f8f2314bb80de9a631db6d5230d3
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-01-31 14:43:50 +0100

    use Card Wrapper to wrap the h1

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import Card from "../UI/Card"
+import classes from "./Home.module.css"
+
+
+const Home = () => {
+    return (
+        <Card className={classes.home}>
+            <h1>Welcome To SHoMooRe!</h1>
+        </Card>
+    )
+}
+
+export default Home
