a8ef5233f1b6e76e4454fc2ae8b8e324e3b400c2
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-03 12:39:21 +0100

    useFragment to output function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useContext } from 'react';
 import ReactDOM from "react-dom"
 import Home from './Components/Home/Home';
 import Logins from './Components/Login/Logins';
@@ -6,46 +6,28 @@ import MainNavBar from './Components/MainNavBar/MainNavBar';
 import Authorize from './Store/authContext';
 
 
+
 function App(props) {
 
-  const [isLoging, setIsLogin] = useState(false)
-
-  useEffect(() => {
-    let storedLoginData = localStorage.getItem("isLogin")
-    if (storedLoginData) {
-      setIsLogin(true)
-    }
-  },[])
-
-  const loginHandler = () => {
-    localStorage.setItem("isLogin", "1")
-    setIsLogin(true)
-  }
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLogin")
-    setIsLogin(false)
-  }
 
   const MainHeaderPage = () => {
     return (<MainNavBar/>)
   }
 
   const LoginPage = () => {
-    return <Logins onLogin={loginHandler} />
+    return <Logins />
   }
+
+  const ctx = useContext(Authorize)
 
 
   return (
-    <Authorize.Provider
-     value={{
-      isLoging: isLoging, 
-      onLogout: logoutHandler}}>
+    <Fragment>
       {ReactDOM.createPortal(<MainHeaderPage />, document.getElementById("mainNavbar"))}
 
-      {!isLoging && ReactDOM.createPortal(<LoginPage onLogin={props.onLogin} />, document.getElementById("loginId"))}
-      {isLoging && <Home onLogout= {logoutHandler} />}
-    </Authorize.Provider>
+      {!ctx.isLoging && ReactDOM.createPortal(<LoginPage />, document.getElementById("loginId"))}
+      {ctx.isLoging && <Home />}
+    </Fragment>
   );
 }
 
