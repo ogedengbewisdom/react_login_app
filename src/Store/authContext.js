593789a06fdda19f77913cf33acda4dfb46e9db6
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-03 12:35:30 +0100

    making AuthorizeProvider reusable and refactoring app component

--- src/Store/authContext.js
+++ src/Store/authContext.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Authorize = React.createContext({
     isLoging: false,
     onLogout: () => {},
     onLogin: () => {}
 })
+
+export const AuthorizeProvider = (props) => {
+    const [isLoging, setIsLogin] = useState(false)
+
+    useEffect( ()=> {
+        const storedLoginData = localStorage.getItem("isLogged")
+        if (storedLoginData) {
+            setIsLogin(true)
+        }
+    },[])
+    
+    const loginHandler = () => {
+        localStorage.setItem("isLogged", "1")
+        setIsLogin(true)
+    }
+
+    const logoutHandler = () => {
+        localStorage.removeItem("isLogged")
+        setIsLogin(false)
+    }
+
+    return (
+        <Authorize.Provider
+          value={{
+            isLoging: isLoging,
+            onLogin: loginHandler,
+            onLogout: logoutHandler
+          }}
+        >
+            {props.children}
+        </Authorize.Provider>
+    )
+}
 
 export default Authorize
