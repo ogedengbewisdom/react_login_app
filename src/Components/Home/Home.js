37195822c78bcbe41b1dbbed2d175e43c6e856af
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-03 12:38:22 +0100

    useContext instead of props

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,19 @@
+import { useContext } from "react"
+import Authorize from "../../Store/authContext"
 import Button from "../UI/Button"
 import Card from "../UI/Card"
 import classes from "./Home.module.css"
 
 
 const Home = (props) => {
+
+    const ctx = useContext(Authorize)
     return (
         <Card className={classes.home}>
             <div>
                 <h1>Welcome To SHoMooRe!</h1>
                 <div className={classes.buton}>
-                    <Button onClick={props.onLogout}>Logout</Button>
+                    <Button onClick={ctx.onLogout}>Logout</Button>
                 </div>
             </div>
         </Card>
